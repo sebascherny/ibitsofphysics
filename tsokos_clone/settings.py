9d8f9e9cebd60a3336e3eddf933f2603f75fd37f
Sebastian Cherny <sebascherny@gmail.com>
2025-05-19 07:33:57 -0300

    0.0.0.0

--- tsokos_clone/settings.py
+++ tsokos_clone/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'changeme')
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,ibitsofphysics.onrender.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,ibitsofphysics.onrender.com,0.0.0.0').split(',')
 
 
 # Application definition
